48333f3be44aef0667ef8f6745201307d54fd82e
halukcan kasım <halukcankasim@hotmail.com>
2023-11-26 19:46:48 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using CodeGenerate.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeGenerate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public bool generateBtnEnabled { get; set; }
+        private string CodeGenerate { get; set; }
+        private string VMCodeGenerate { get; set; }
+        public string SelectedDataBase { get; set; }
+        public string SelectedShema { get; set; }
+        public string SelectedTable { get; set; }
+        public ObservableCollection<string> DatabaseNames { get; set; }
+
+        public ObservableCollection<string> tableNames;
+
+        public ObservableCollection<string> TableNames
+        {
+            get
+            {
+                return tableNames;
+            }
+            set
+            {
+                tableNames = value;
+                OnPropertyChanged("TableNames");
+            }
+        }
+
+
+        public ObservableCollection<string> shemasName;
+
+        public ObservableCollection<string> ShemasNames
+        {
+            get
+            {
+                return shemasName;
+            }
+            set
+            {
+                shemasName = value;
+                OnPropertyChanged("ShemasNames");
+            }
+        }
+
+
+        private MainVM vm;
+        public MainWindow()
+        {
+            vm = new MainVM();
+            DatabaseNames =  vm.GetDatabaseName();
+
+            SelectedDataBase = "";
+            SelectedShema = "";
+            SelectedTable = "";
+            CodeGenerate = "";
+            VMCodeGenerate = "";
+            generateBtnEnabled = false;
+
+            TableNames = new ObservableCollection<string>();
+            ShemasNames = new ObservableCollection<string>();
+
+            InitializeComponent();
+        }
+        private void dbComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ComboBox).SelectedItem != null)
+            {
+                SelectedDataBase = (sender as ComboBox).SelectedItem.ToString();
+
+                ShemasNames.Clear();
+
+                ShemasNames = vm.GetShemasNames(SelectedDataBase);
+            }
+            
+        }
+
+        private void SHComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ComboBox).SelectedItem != null)
+            {
+                SelectedShema = (sender as ComboBox).SelectedItem.ToString();
+
+                TableNames.Clear();
+                TableNames = vm.GetTablesNames(SelectedDataBase, SelectedShema);
+            }
+
+        }
+        private void TBComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ComboBox).SelectedItem != null)
+            {
+                SelectedTable = (sender as ComboBox).SelectedItem.ToString();
+            }
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(SelectedTable))
+            {
+                try
+                {
+                    CodeGenerate = vm.CodeGenerate(SelectedDataBase, SelectedShema, SelectedTable);
+                    CreateCodeCs();
+
+                    VMCodeGenerate = vm.VMCodeGenerate(SelectedDataBase, SelectedShema, SelectedTable);
+                    VMCreateCodeCs();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tablo Seçilmeli");
+            }
+        }
+
+        private void CreateCodeCs()
+        {
+            string path = "D:\\Source\\CodeGenerate";
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
+
+            string currentPath = path + $"\\{SelectedTable}.cs";
+            FileStream fs;
+
+            if (!File.Exists(currentPath))
+            {
+                fs = new FileStream(currentPath, FileMode.Create);
+            }
+            else
+            {
+                fs = new FileStream(currentPath, FileMode.Append);
+            }
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(CodeGenerate);
+            sw.Close();
+        }
+
+        private void VMCreateCodeCs()
+        {
+            string path = "D:\\Source\\CodeGenerate";
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
+
+            string currentPath = path + $"\\{SelectedTable}VM.cs";
+            FileStream fs;
+
+            if (!File.Exists(currentPath))
+            {
+                fs = new FileStream(currentPath, FileMode.Create);
+            }
+            else
+            {
+                fs = new FileStream(currentPath, FileMode.Append);
+            }
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(VMCodeGenerate);
+            sw.Close();
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ ViewModel/MainVM.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Data;
+using System.CodeDom.Compiler;
+
+namespace CodeGenerate.ViewModel
+{
+    public class MainVM
+    {
+        private SqlConnection cnn;
+        private ObservableCollection<string> DBNames;
+        private ObservableCollection<string> TableNames;
+
+        public MainVM() 
+        {
+        }
+
+        private void OpenDatabase()
+        {
+            try
+            {
+                cnn = new SqlConnection("server=LAPTOP-S65PUQCG\\SQLEXPRESS01;integrated security=true");
+                cnn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Database bağlanırken sorun oluştu!.\n{ex.Message}", "Error");
+            }
+        }
+
+        private void OpenDatabaseWithDBname(string dbName)
+        {
+            try
+            {
+                cnn = new SqlConnection($"server=LAPTOP-S65PUQCG\\SQLEXPRESS01; database={dbName}; integrated security=true");
+                cnn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Database bağlanırken sorun oluştu!.\n{ex.Message}", "Error");
+            }
+        }
+
+        public ObservableCollection<string> GetDatabaseName()
+        {
+
+            DBNames = new ObservableCollection<string>();
+            OpenDatabase();
+            if (cnn.State == ConnectionState.Open)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cnn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT name from sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');";
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    string tableName = dr["name"].ToString();
+                    DBNames.Add(tableName);
+                }
+                cnn.Close();
+            }
+            return DBNames;
+        }
+
+        public ObservableCollection<string> GetTablesNames(string dbNames, string shema)
+        {
+
+            TableNames = new ObservableCollection<string>();
+            OpenDatabaseWithDBname(dbNames);
+            if (cnn.State == ConnectionState.Open)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cnn;
+                foreach (var tableName in cnn.GetSchema("Tables").AsEnumerable().Where(x => x[1].ToString() == shema).Select(s => s[2].ToString()).ToList())
+                {
+                    TableNames.Add(tableName);
+                }
+                cnn.Close();
+            }
+            return TableNames;
+        }
+
+
+
+        public ObservableCollection<string> GetShemasNames(string dbNames)
+        {
+
+            TableNames = new ObservableCollection<string>();
+            OpenDatabaseWithDBname(dbNames);
+            if (cnn.State == ConnectionState.Open)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cnn;
+                foreach (var tableName in cnn.GetSchema("Tables").AsEnumerable().Select(s => s[1].ToString()).Distinct().ToList())
+                {
+                    TableNames.Add(tableName);
+                }
+                cnn.Close();
+            }
+            return TableNames;
+        }
+
+
+
+        public string CodeGenerate(string dbNames,string shemaName, string tableName)
+        {
+            string codeGenerate = "";
+            OpenDatabaseWithDBname(dbNames);
+            if (cnn.State == ConnectionState.Open)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cnn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = $"[{shemaName}].[ModelCodeGenerate]";
+                    cmd.Parameters.Add("@TableName", SqlDbType.VarChar).Value = tableName;
+                    cmd.Parameters.Add("@TableSchema", SqlDbType.VarChar).Value = shemaName;
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        codeGenerate = dr["result"].ToString();
+                    }
+                    cnn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            return codeGenerate;
+        }
+
+
+        public string VMCodeGenerate(string dbNames, string shemaName, string tableName)
+        {
+            string codeGenerate = "";
+            OpenDatabaseWithDBname(dbNames);
+            if (cnn.State == ConnectionState.Open)
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cnn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = $"[{shemaName}].[VMModelCodeGenerate]";
+                    cmd.Parameters.Add("@TableName", SqlDbType.VarChar).Value = tableName;
+                    cmd.Parameters.Add("@TableSchema", SqlDbType.VarChar).Value = shemaName;
+                    cmd.Parameters.Add("@Database", SqlDbType.VarChar).Value = dbNames;
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        codeGenerate = dr["result"].ToString();
+                    }
+                    cnn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            return codeGenerate;
+        }
+    }
+}
